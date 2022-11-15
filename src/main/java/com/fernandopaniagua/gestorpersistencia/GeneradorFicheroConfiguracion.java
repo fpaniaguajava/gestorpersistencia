750a318a468f6abbbf759ff8545ade3bf9b1ce4c
fpaniaguaangular <fernando.paniagua.java@gmail.com>
2022-11-15 21:50:18 +0100

    Update GeneradorFicheroConfiguracion.java

--- src/main/java/com/fernandopaniagua/gestorpersistencia/GeneradorFicheroConfiguracion.java
+++ src/main/java/com/fernandopaniagua/gestorpersistencia/GeneradorFicheroConfiguracion.java
@@ -1,7 +1,6 @@
 package com.fernandopaniagua.gestorpersistencia;
 
 import java.io.FileOutputStream;
-import java.io.OutputStream;
 import java.util.Properties;
 
 public class GeneradorFicheroConfiguracion {
